eff416b41073d298c78b6841a3996ea6f4d65fdd
SkocicD <skocicdavid@gmail.com>
2025-09-10 19:41:28 -0400

    Added week 1 challenges

---
+++ week1/weekly_challenge/easy.py
@@ -0,0 +1,18 @@
+'''
+The goal of this challenge is to take an array of 100 values and turn it into a 2D array in the format
+[
+[1,2,3,4,5,7,8,9,10]
+[11,12,13,14,15 ...
+.
+.
+.
+..............., 100]
+]
+'''
+
+# an array that looks something like [1,2,3,4,5, ... , 100]
+arr = [i for i in range(1, 101)]
+
+end = []
+
+# Your Code Here:
---
+++ week1/weekly_challenge/medium.py
@@ -0,0 +1,20 @@
+'''
+The goal of this challenge is to take an array of 100 values and turn it into a 2D array in the format
+[
+[1,2,3,4,5,7,8,9,10]
+[11,12,13,14,15 ...
+.
+.
+.
+..............., 100]
+]
+'''
+import random
+
+# an array that looks something like [1,2,3,4,5, ... , 100]
+arr = [i for i in range(1, 101)]
+random.shuffle(arr)
+
+end = []
+
+# Your Code Here:
