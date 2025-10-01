2b89cbb152166326d8daacc0df8bf3f44056d323
SkocicD <skocicdavid@gmail.com>
2025-10-01 17:54:42 -0400

    did week 2 first example

--- week2/tcp/receiver.py
+++ week2/tcp/receiver.py
@@ -2,9 +2,9 @@ import time
 import socket
 
 # FILL IN... what is the host for your own computer?
-host =
+host = '127.0.0.1'
 # FILL IN... YOU PICK THIS PORT between 50000 and 65535
-port =
+port = 50001
 
 # Create a TCP/IP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,7 +18,5 @@ conn, addr = s.accept()  # Accept a connection
 print(f"Connected by {addr}")
 while True:
     data = conn.recv(1024)  # Receive data (max 1024 bytes at a time)
-    if not data:
-        break
     print(f"Received: {data.decode('utf-8')}")
     time.sleep(.5)
--- week2/tcp/sender.py
+++ week2/tcp/sender.py
@@ -1,9 +1,9 @@
-import socket.socket as socket
+import socket
 
 # FILL IN... what is the host for your own computer?
-host = xxx
+host = 'localhost'
 # YOU PICK THIS PORT between 50000 and 65535
-port = xxx
+port = 50001
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +12,7 @@ s.connect((host, port))  # Connect to the server
 print(f"Connected to {host}:{port}")
 
 # fill in the message you want to send
-message = ''
+message = 'david'
 
 s.sendall(message.encode('utf-8'))  # Send a message
 print(f"Sent: {message}")
