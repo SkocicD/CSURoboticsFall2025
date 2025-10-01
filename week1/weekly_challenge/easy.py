3e9db7aab65c59b2da2b9d9a209341f2e3e5e76a
William Rosenberg <wrosenberg2000@gmail.com>
2025-10-01 23:52:24 +0000

    10/1/2025 Commit of easy

--- week1/weekly_challenge/easy.py
+++ week1/weekly_challenge/easy.py
@@ -16,3 +16,11 @@ arr = [i for i in range(1, 101)]
 end = []
 
 # Your Code Here:
+num_lists = 10
+def split(arr, num_lists):
+	length = len(arr)
+	return [ arr[i*length // num_lists: (i+1)*length // num_lists]
+		for i in range(num_lists) ] 
+block = split(arr,10) 
+for i in block:
+	print(i)
